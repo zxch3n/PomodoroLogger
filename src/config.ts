be90c9e087495877cdf1ade3cb230ccaf06095a1
DESKTOP-VOMF6EQ\Remch <remch183@outlook.com>
2019-08-10 12:47:05 +0800

    Fix #21 save data to appdata

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,15 @@
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 
-export const dbBaseDir = process.env.NODE_ENV !== 'test' ? './db' : './__test__db';
+export const baseDir =
+    process.env.APPDATA ||
+    (process.platform === 'darwin'
+        ? process.env.HOME + 'Library/Preferences'
+        : process.env.HOME + '/.local/share');
+
+const dbDir = process.env.NODE_ENV !== 'test' ? 'db' : '__test__db';
+export const dbBaseDir =
+    process.env.NODE_ENV === 'production' ? join(baseDir, dbDir) : './' + dbDir;
 
 if (!existsSync(dbBaseDir)) {
     mkdirSync(dbBaseDir);
