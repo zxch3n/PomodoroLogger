6dd6fd06ae9ee5dcfbd2f6e617fdcb65607e5d30
Zixuan Chen <remch183@outlook.com>
2020-09-21 00:42:21 +0800

    feat: use new search entry

--- src/renderer/style/form.ts
+++ src/renderer/style/form.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const LabelButton = styled.span`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: center;
-  min-height: 32px;
-  
-  button {
-    margin: 0 4px;
-  }
+    display: flex;
+    align-items: flex-start;
+    flex-direction: row;
+    justify-content: center;
+    min-height: 32px;
+
+    button {
+        margin: 0 4px;
+    }
 `;
